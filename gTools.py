02dfcf3ce0c3c95c79305b5afb6f6c293de7b90b
Petr Cagas <pcagas@vt.edu>
2017-03-10 17:51:50 -0500

    gplot.py: slicing

--- gTools.py
+++ gTools.py
@@ -5,6 +5,7 @@ Postgkyl module with random useful stuff :)
 
 import numpy
 import matplotlib.pyplot as plt
+import math
 from scipy.signal import butter, lfilter, freqz
 
 def rotationMatrix(vector):
@@ -36,6 +37,70 @@ def rotationMatrix(vector):
 
     return rot
 
+def findNearest(array, value):
+    idx = numpy.searchsorted(array, value)
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) 
+                    < math.fabs(value - array[idx])):
+        return array[idx-1]
+    else:
+        return array[idx]
+def findNearestIdx(array, value):
+    idx = numpy.searchsorted(array, value)
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) 
+                    < math.fabs(value - array[idx])):
+        return idx-1
+    else:
+        return idx
+
+def fixCoordSlice(coords, values,
+                  fix1=None, fix2=None, fix3=None,
+                  fix4=None, fix5=None, fix6=None):
+    """Fix specified coordinates and decrease dimensionality
+
+    Parameters:
+    coords -- array of coordinates
+    values -- array of field values
+    fix1 -- fixes the first coordinate to provided index (default None)
+    fix2 -- fixes the second coordinate to provided index (default None)
+    fix3 -- fixes the third coordinate to provided index (default None)
+    fix4 -- fixes the fourth coordinate to provided index (default None)
+    fix5 -- fixes the fifth coordinate to provided index (default None)
+    fix6 -- fixes the sixth coordinate to provided index (default None)
+
+    Returns:
+    coordsOut -- coordinates with decreased number of dimensions
+    valuesOut -- field values with decreased number of dimensions
+
+    Example:
+    By fixing an x-index (fix1), 1X1V simulation data transforms
+    to 1D velocity profile.
+
+    Note:
+    Fixing higher dimensions than available in the data has no effect.
+    """
+    fix = (fix1, fix2, fix3, fix4, fix5, fix6)
+    coordsOut = numpy.copy(coords)
+    valuesOut = numpy.copy(values)
+    for i, val in reversed(list(enumerate(fix))):
+        if val is not None and len(values.shape) > i:
+            # turn N-D coords into correct 1D coord array
+            temp = coords[i]
+            coords1D = numpy.linspace(temp.min(), temp.max(),
+                                      temp.shape[i])
+            idx = findNearestIdx(coords1D, float(val))
+            print(idx)
+            # create for mask compressing
+            mask = numpy.zeros(values.shape[i])
+            mask[int(idx)] = 1
+            # delete coordinate matrices for the fixed coordinate
+            coordsOut = numpy.delete(coordsOut, i, 0)
+            coordsOut = numpy.compress(mask, coordsOut, axis=i+1)  
+            coordsOut = numpy.squeeze(coordsOut)
+
+            valuesOut = numpy.compress(mask, valuesOut, axis=i) 
+            valuesOut = numpy.squeeze(valuesOut)
+    return coordsOut, valuesOut
+
 def clickCoords(event):
     global ix, iy
     ix, iy = event.xdata, event.ydata
@@ -44,11 +109,9 @@ def clickCoords(event):
 def fftFiltering(data, dt=1, cutoff=None):
     """Filter data using numpy FFT.
 
-    Inputs:
+    Parameters:
     data -- numpy array of data
-
-    Key words:
-    dt     -- set spacing of data (default: 1)
+    dt -- set spacing of data (default: 1)
     cutoff -- set high frequency cut-off (default: None)
 
     Note:
@@ -81,27 +144,25 @@ def fftFiltering(data, dt=1, cutoff=None):
     FT[freq < -cutoff] = 0
     return numpy.fft.ifft(FT) 
 
-def butter_lowpass(cutoff, fs, order=5):
+def butterLowpass(cutoff, fs, order=5):
     nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    normalCutoff = cutoff / nyq
+    b, a = butter(order, normalCutoff, btype='low', analog=False)
     return b, a
 
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
+def butterLowpassFilter(data, cutoff, fs, order=5):
+    b, a = butterLowpass(cutoff, fs, order=order)
     y = lfilter(b, a, data)
     return y
 
 def butterFiltering(data, dt, cutoff):
     """Filter data using Butterworth filter
 
-    Inputs:
+    Parameters:
     data -- numpy array of data
-
-    Key words:
-    dt     -- set spacing of data (default: 1)
+    dt -- set spacing of data (default: 1)
     cutoff -- set high frequency cut-off (in Hz)
     """
     order = 6
     fs = 1/dt # sample rate
-    return butter_lowpass_filter(data, cutoff, fs, order)
+    return butterLowpassFilter(data, cutoff, fs, order)
