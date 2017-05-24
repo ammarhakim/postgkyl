7305dd601b2084713943899ac4b13408aba5f148
Petr Cagas <pcagas@vt.edu>
2017-05-24 17:15:16 -0400

    New stack design

--- postgkyl/commands/info.py
+++ postgkyl/commands/info.py
@@ -1,19 +1,18 @@
 import click
 
+from postgkyl.tools.stack import pushStack, pullStack, popStack
+
 @click.command(help='Print data info')
 @click.pass_context
-def info(ctx):
-    numData = len(ctx.obj['values'])
-
-    click.echo('\nPrinting info:')
-    for f in range(numData):
-        click.echo(' * Dataset #{:d}'.format(f))
-        click.echo('  * Time: {:f}'.format(ctx.obj['data'][f].time))
-        click.echo('  * Dumber of components: {:d}'.
-                   format(ctx.obj['values'][f].shape[-1]))
-        numDims = len(ctx.obj['values'][f].shape)-1
+def info(ctx):                                    
+    click.echo('\nPrinting the current top of stack:')
+    for s in range(ctx.obj['numSets']):
+        coords, values = pullStack(ctx, s)
+        click.echo(' * Dataset #{:d}'.format(s))
+        click.echo('  * Time: {:f}'.format(ctx.obj['data'][s].time))
+        click.echo('  * Dumber of components: {:d}'.format(values.shape[-1]))
+        numDims = len(values.shape)-1
         click.echo('  * Dimensions ({:d}):'.format(numDims))
         for d in range(numDims):
             click.echo('   * Dim {:d}: Num. Cells: {:d}; Lower: {:f}; Upper: {:f}'.
-              format(d, len(ctx.obj['coords'][f][d]),
-                     ctx.obj['coords'][f][d][0], ctx.obj['coords'][f][d][-1]))
+                       format(d, len(coords[d]), coords[d][0], coords[d][-1]))
