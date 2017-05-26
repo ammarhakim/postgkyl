a5c8abd693ff0926d7d00ebbd263510fe08deb47
Petr Cagas <pcagas@vt.edu>
2017-05-26 14:23:24 -0400

    CLI labels might work

--- postgkyl/commands/output.py
+++ postgkyl/commands/output.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from postgkyl.tools.stack import pushStack, pullStack, popStack
+from postgkyl.tools.stack import peakStack, peakLabel, getFullLabel
 
 dirPath = os.path.dirname(os.path.realpath(__file__))
 
@@ -52,19 +52,32 @@ def plot(ctx, show, style, axismode, save):
     plt.style.use(style)
     numSets = ctx.obj['numSets']
     for s in range(numSets):
-        coords, values = pullStack(ctx, s)
+        coords, values = peakStack(ctx, s)
+
+        label = peakLabel(ctx, s)
+        title = peakLabel(ctx, s, 0)
+        if label == title:
+            label = ''
 
         numDims = len(coords)
         numComps = values.shape[-1]
 
         for comp in range(numComps):
             fig, ax = _getFig(ctx)
+
+            if numComps > 1:
+                labelComp = '{:s} c{:d}'.format(label, comp)
+            else:
+                labelComp = label
+
             if numDims == 1:
-                im = ax.plot(coords[0], values[..., comp])
+                im = ax.plot(coords[0], values[..., comp],
+                             label=labelComp)
                 plt.autoscale(enable=True, axis='x', tight=True)
             elif numDims == 2:
                 im = ax.pcolormesh(coords[0], coords[1],
-                                   values[..., comp].transpose())
+                                   values[..., comp].transpose(),
+                                   label=labelComp)
                 _colorbar(im, ax, fig)
                 ax.axis(axismode)
             else:
@@ -72,14 +85,27 @@ def plot(ctx, show, style, axismode, save):
                            format(numDims))
                 ctx.exit()
 
+            if ctx.obj['hold'] == 'on':
+                ax.set_title('{:s}'.format(title))
+                ax.legend(loc=0)
+            else:
+                ax.set_title('{:s} {:s}\n{:f}'.format(title, labelComp))
+
             ax.grid(True)
             plt.tight_layout()
 
+            if numComps > 1 and ctx.obj['hold'] == 'off':
+                saveName = '{:s}_c{:d}.png'.format(getFullLabel(ctx, s),
+                                                   comp)
+            else:
+                saveName = '{:s}.png'.format(getFullLabel(ctx, s))
+            if save and ctx.obj['hold'] == 'off':
+                fig.savefig(saveName)
+
+    if save and ctx.obj['hold'] == 'on':
+        fig.savefig(saveName)
     if show:
         plt.show()
-
-    if save:
-        fig.savefig('placeholder.png')
 
 @click.command(help='Hold the plotting')
 @click.option('--on', 'hld', flag_value='on', default=True,
@@ -98,7 +124,7 @@ def hold(ctx, hld):
 def info(ctx):                                    
     click.echo('\nPrinting the current top of stack info:')
     for s in range(ctx.obj['numSets']):
-        coords, values = pullStack(ctx, s)
+        coords, values = peakStack(ctx, s)
         click.echo(' * Dataset #{:d}'.format(s))
         click.echo('  * Time: {:f}'.format(ctx.obj['data'][s].time))
         click.echo('  * Dumber of components: {:d}'.format(values.shape[-1]))
