c81ee8eb7d28b915a8fb491628ea873c4b79c82b
ammarhakim <a.hakim777@gmail.com>
2017-09-05 15:51:27 -0400

    Added code to compute P_par and P_perp. However, this is not 100% what we want. The command line truly horrific:
    pgkyl -f ot-10m-local-diffusion_q_250.h5 -f ot-10m-local-diffusion_q_250.h5 dataset -i 0 comp 0:10 tenmoment -v pressureTensor dataset -i 1 comp 13:16 dataset -a cglpressure dataset -i 0 plot
    
    But it works!

--- postgkyl/commands/tenmoment.py
+++ postgkyl/commands/tenmoment.py
@@ -1,70 +1,9 @@
 import click
 import numpy as np
+import tm
 
 from postgkyl.tools.stack import pushStack, peakStack, popStack, antiSqueeze
 from postgkyl.commands.output import vlog
-
-def getRho(q):
-    return q[...,0]
-def getU(q):
-    return q[...,1]/q[...,0]
-def getV(q):
-    return q[...,2]/q[...,0]
-def getW(q):
-    return q[...,3]/q[...,0]
-
-def getPxx(q):
-    r = getRho(q)
-    u = getU(q)
-    return q[...,4] - r*u*u
-
-def getPxy(q):
-    r = getRho(q)
-    u = getU(q)
-    v = getV(q)
-    return q[...,5] - r*u*v
-
-def getPxz(q):
-    r = getRho(q)
-    u = getU(q)
-    w = getW(q)
-    return q[...,6] - r*u*w
-
-def getPyy(q):
-    r = getRho(q)
-    v = getV(q)
-    return q[...,7] - r*v*v
-
-def getPyz(q):
-    r = getRho(q)
-    v = getV(q)
-    w = getW(q)
-    return q[...,8] - r*v*w
-
-def getPzz(q):
-    r = getRho(q)
-    w = getW(q)
-    return q[...,9] - r*w*w
-
-def getVel(q):
-    tmp = np.copy(q[..., 1:4])
-    tmp[..., 0] = getU(q)
-    tmp[..., 1] = getV(q)
-    tmp[..., 2] = getW(q)
-    return tmp
-
-def getPressureTensor(q):
-    tmp = np.copy(q[..., 4:10])
-    tmp[..., 0] = getPxx(q)
-    tmp[..., 1] = getPxy(q)
-    tmp[..., 2] = getPxz(q)
-    tmp[..., 3] = getPyy(q)
-    tmp[..., 4] = getPyz(q)
-    tmp[..., 5] = getPzz(q)
-    return tmp
-
-def getPressure(q):
-    return (getPxx(q)+getPyy(q)+getPzz(q))/3.0
 
 @click.command(help='Extract ten-moment primitive variables from fluid simulation')
 @click.option('-v', '--variable_name', help="Variable to plot", prompt=True,
@@ -82,31 +21,31 @@ def tenmoment(ctx, variable_name):
 
         vlog(ctx, 'euler: Extracting {:s} from data set #{:d}'.format(v, s))
         if v == "density":
-            tmp = getRho(q)
+            tmp = tm.getRho(q)
         elif v == "xvel":
-            tmp = getU(q)
+            tmp = tm.getU(q)
         elif v == "yvel":
-            tmp = getV(q)
+            tmp = tm.getV(q)
         elif v == "zvel":
-            tmp = getW(q)
+            tmp = tm.getW(q)
         elif v == "vel":
-            tmp = getVel(q)
+            tmp = tm.getVel(q)
         elif v == "pxx":
-            tmp = getPxx(q)
+            tmp = tm.getPxx(q)
         elif v == "pxy":
-            tmp = getPxy(q)
+            tmp = tm.getPxy(q)
         elif v == "pxz":
-            tmp = getPxz(q)
+            tmp = tm.getPxz(q)
         elif v == "pyy":
-            tmp = getPyy(q)
+            tmp = tm.getPyy(q)
         elif v == "pyz":
-            tmp = getPyz(q)
+            tmp = tm.getPyz(q)
         elif v == "pzz":
-            tmp = getPzz(q)
+            tmp = tm.getPzz(q)
         elif v == "pressure":
-            tmp = getPressure(q)
+            tmp = tm.getPressure(q)
         elif v == "pressureTensor":
-            tmp = getPressure(q)
+            tmp = tm.getPressureTensor(q)
         else:
             vlog(ctx, 'No such variable %s' % v)
             
