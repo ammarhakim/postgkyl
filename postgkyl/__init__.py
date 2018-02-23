22d15de2b5c44708bd2038968f41a1de0233ca9e
Petr Cagas <pcagas@vt.edu>
2018-02-22 22:28:05 -0500

    history loading fixed

--- postgkyl/__init__.py
+++ postgkyl/__init__.py
@@ -35,7 +35,6 @@ from . import output
 
 # import selected classes to the root
 from .data.gdata import GData
-from .data.dg import GInterpZeroOrder
 from .data.dg import GInterpNodal
 from .data.dg import GInterpModal
 
