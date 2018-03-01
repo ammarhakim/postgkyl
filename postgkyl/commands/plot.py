e89117538df790b1d5ba2aad1eb54b6ebcafab03
Petr Cagas <pcagas@vt.edu>
2018-03-01 16:17:56 -0500

    additional plot options

--- postgkyl/commands/plot.py
+++ postgkyl/commands/plot.py
@@ -10,6 +10,8 @@ from postgkyl.commands.util import vlog, pushChain
               help="Specify figure to plot in.")
 @click.option('--squeeze', '-s', is_flag=True,
               help="Squeeze the components into one panel.")
+@click.option('-a', '--arg', type=click.STRING,
+              help="Additional plotting arguments like '*--'.")
 @click.option('-c', '--contour', is_flag=True,
               help="Switch to contour mode.")
 @click.option('-q', '--quiver', is_flag=True,
@@ -31,6 +33,12 @@ from postgkyl.commands.util import vlog, pushChain
               help="Turn showing of the plot ON and OFF (default: ON).")
 #@click.option('--color', type=click.STRING,
 #              help="Set color when available.")
+@click.option('-x', '--xlabel', type=click.STRING,
+              help="Specify a x-axis label.")
+@click.option('-y', '--ylabel', type=click.STRING,
+              help="Specify a y-axis label.")
+@click.option('-t', '--title', type=click.STRING,
+              help="Specify a title label.")
 @click.option('--save', is_flag=True,
               help="Save figure as PNG.")
 @click.option('--saveas', type=click.STRING, default=None,
@@ -44,8 +52,12 @@ def plot(ctx, **kwargs):
         dat = ctx.obj['dataSets'][s]
         #if kwargs['color'] == 'seq':
         #    kwargs['color'] = cm.inferno(s/len(ctx.obj['sets']))
-        postgkyl.output.plot(dat, labelPrefix='s{:d}'.format(s),
-                             **kwargs)
+        if kwargs['arg'] is not None:
+            postgkyl.output.plot(dat, kwargs['arg'], labelPrefix='s{:d}'.format(s),
+                                 **kwargs)
+        else:
+            postgkyl.output.plot(dat, labelPrefix='s{:d}'.format(s),
+                                 **kwargs)
         if kwargs['save'] or kwargs['saveas']:
             if kwargs['saveas']:
                 fName = kwargs['saveas']
