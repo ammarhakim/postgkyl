db00072b51b13ff9100d5aaccf8fc1711fdbdc5c
Petr Cagas <pcagas@vt.edu>
2018-03-05 09:35:45 -0500

    added fft

---
+++ postgkyl/commands/fft.py
@@ -0,0 +1,19 @@
+import click
+
+from postgkyl.diagnostics import Fft
+from postgkyl.commands.util import vlog, pushChain
+
+@click.command(help='Calculate the Fast Fourier Transformartion.')
+@click.option('-p', '--psd', is_flag=True,
+              help="Limits output to positive frequencies and returns the power spectral density |FT|^2.")
+@click.pass_context
+def fft(ctx, **kwargs):
+    vlog(ctx, 'Starting FFT')
+    pushChain(ctx, 'fft.fft', **kwargs)
+
+    for s in ctx.obj['sets']:
+        data = ctx.obj['dataSets'][s]
+        ft = Fft(data)
+        ft.fft(kwargs['psd'], stack=True)
+        
+    vlog(ctx, 'Finishing FFT')
