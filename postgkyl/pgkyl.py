fd1b3a650929cfbb7e93abb4b4ca71c98984c970
Petr Cagas <pcagas@vt.edu>
2018-08-31 09:02:09 -0400

    version increse

--- postgkyl/pgkyl.py
+++ postgkyl/pgkyl.py
@@ -15,7 +15,7 @@ import postgkyl.commands as cmd
 def _printVersion(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
-    click.echo('Postgkyl 1.0.4 2018-05-02 ({:s})'.format(sys.platform))
+    click.echo('Postgkyl 1.0.7 2018-06-29 ({:s})'.format(sys.platform))
     click.echo(sys.version)
     click.echo('Copyright 2016-2018 Gkyl Team')
     click.echo('Gkyl can be used freely for research at universities,')
