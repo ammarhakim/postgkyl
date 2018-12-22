9d4227fe92cb362f0d9e9fe6bddc0e17610f073a
Ammar <11265732+ammarhakim@users.noreply.github.com>
2018-12-22 13:19:40 -0500

    - Updated license to 2019; Removed old stuff from README.md
    - Adding install script for 'dash' package to allow eventual web-based
      interface to pgkyl

---
+++ proto/dash-install.sh
@@ -0,0 +1,4 @@
+pip install dash==0.34.0  # The core dash backend
+pip install dash-html-components==0.13.4  # HTML components
+pip install dash-core-components==0.41.0  # Supercharged components
+pip install dash-table==3.1.11  # Interactive DataTable component (new!)
