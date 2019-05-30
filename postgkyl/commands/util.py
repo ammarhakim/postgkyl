fdbbad41f6384c26b09062d341e05d01762543b6
Petr Cagas <pcagas@vt.edu>
2019-05-29 23:50:27 -0400

    added optional file names for chains

--- postgkyl/commands/util.py
+++ postgkyl/commands/util.py
@@ -11,17 +11,24 @@ def vlog(ctx, message):
                                fg='green'))
 
 def pushChain(ctx, command, **kwargs):
+    print(ctx.obj['savechainPath'])
     if ctx.obj['savechain'] == True:
-        fh = open('.pgkylchain', 'a')
+        fh = open(ctx.obj['savechainPath'], 'a')
         s = '{:s}'.format(command)
 
         for key, value in kwargs.items():
             if sys.version_info[0] == 2:
                 if type(value) == unicode:
                     value = str(value)
+                #end
+            #end
             if type(value) == str:
                 value = "'" + value + "'"
+            #end
             s = s + ', {:s}={}'.format(key, value)
+        #end
         fh.write(s + '\n')
         fh.close()
+    #end
+#end
 
