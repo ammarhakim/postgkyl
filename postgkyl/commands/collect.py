569cbf6901e1531ceff561c2e7cfe04194d0bfe2
Noah Mandell <nrmandell@gmail.com>
2019-05-30 22:57:55 -0400

    adding a -g/--group flag to collect, which will collect each group of matching files separately, based on the name of the files. example: pgkyl -f "a_ion_*.bp" -f "a_electron_*.bp" collect -g ... this will create two new active datasets, one collecting "a_ion_*.bp", and another collecting "a_electron_*.bp"

--- postgkyl/commands/collect.py
+++ postgkyl/commands/collect.py
@@ -11,49 +11,70 @@ from postgkyl.commands.util import vlog, pushChain
               help="Specify a period to create epoch data instead of time data")
 @click.option('-o', '--offset', default=0.0, type=click.FLOAT,
               help="Specify an offset to create epoch data instead of time data (default: 0)")
+@click.option('-g', '--group', is_flag=True,
+              help="Separately collect matching files into different sets")
 @click.pass_context
 def collect(ctx, **kwargs):
     vlog(ctx, 'Starting collect')
     pushChain(ctx, 'collect', **kwargs)
-    time = []
-    values = []
+    stems = []
+    activeSets = []
 
-    for s in ctx.obj['sets']:
-        time.append(ctx.obj['dataSets'][s].time)
-        v = ctx.obj['dataSets'][s].getValues()
-        if kwargs['sumdata']:
-            numDims = ctx.obj['dataSets'][s].getNumDims()
-            axis = tuple(range(numDims))
-            values.append(np.nansum(v, axis=axis))
-        else:
-            values.append(v)
-    time = np.array(time)
-    values = np.array(values)
-
-    if kwargs['period'] is not None:
-        time = (time - kwargs['offset']) % kwargs['period']
-
-    sortIdx = np.argsort(time)
-    time = time[sortIdx]
-    values = values[sortIdx]
-
-    if kwargs['sumdata']:
-        grid = [time]
+    group = kwargs['group']
+    
+    if group:
+        for s in ctx.obj['sets']:
+            stem = "_".join(ctx.obj['dataSets'][s].fName.split("_")[:-1])
+            if not stem in stems:
+                stems.append(stem)
     else:
-        s = ctx.obj['sets'][0]
-        grid = list(ctx.obj['dataSets'][s].getGrid())
-        grid.insert(0, time)
+        stems = ['']
 
-    vlog(ctx, 'collect: Creating {:d}D data with shape {}'.format(len(grid), values.shape))
-    idx = len(ctx.obj['dataSets'])
-    ctx.obj['setIds'].append(idx)
-    ctx.obj['dataSets'].append(GData())
-    ctx.obj['labels'].append('collect')
-    ctx.obj['dataSets'][idx].pushGrid(grid)
-    ctx.obj['dataSets'][idx].pushValues(values)
-    ctx.obj['dataSets'][idx].fName = 'collect'
+    for st in stems:
+        if group:
+            vlog(ctx, 'collect: collecting files matching stem {:s}'.format(st))
+        time = []
+        values = []
 
-    vlog(ctx, 'collect: Active data set switched to #{:d}'.format(idx))
-    ctx.obj['sets'] = [idx]
+        for s in ctx.obj['sets']:
+            stem = "_".join(ctx.obj['dataSets'][s].fName.split("_")[:-1])
+            if st == stem or not group:
+                time.append(ctx.obj['dataSets'][s].time)
+                v = ctx.obj['dataSets'][s].getValues()
+                if kwargs['sumdata']:
+                    numDims = ctx.obj['dataSets'][s].getNumDims()
+                    axis = tuple(range(numDims))
+                    values.append(np.nansum(v, axis=axis))
+                else:
+                    values.append(v)
+        time = np.array(time)
+        values = np.array(values)
+
+        if kwargs['period'] is not None:
+            time = (time - kwargs['offset']) % kwargs['period']
+
+        sortIdx = np.argsort(time)
+        time = time[sortIdx]
+        values = values[sortIdx]
+
+        if kwargs['sumdata']:
+            grid = [time]
+        else:
+            s = ctx.obj['sets'][0]
+            grid = list(ctx.obj['dataSets'][s].getGrid())
+            grid.insert(0, time)
+
+        vlog(ctx, 'collect: Creating {:d}D data with shape {}'.format(len(grid), values.shape))
+        idx = len(ctx.obj['dataSets'])
+        ctx.obj['setIds'].append(idx)
+        ctx.obj['dataSets'].append(GData())
+        ctx.obj['labels'].append('collect')
+        ctx.obj['dataSets'][idx].pushGrid(grid)
+        ctx.obj['dataSets'][idx].pushValues(values)
+        ctx.obj['dataSets'][idx].fName = 'collect'
+        vlog(ctx, 'collect: activated data set #{:d}'.format(idx))
+        activeSets.append(idx)
+
+    ctx.obj['sets'] = activeSets
 
     vlog(ctx, 'Finishing collect')
