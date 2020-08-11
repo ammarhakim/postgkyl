9d6b018ffaf31fc72f10e091ba19769b2fcbcb84
Petr Cagas <pcagas@vt.edu>
2020-08-11 11:07:54 -0400

    Almost fixing the p=2 assumtion

--- postgkyl/modalDG/interpolate.py
+++ postgkyl/modalDG/interpolate.py
@@ -20,15 +20,23 @@ def interpolate(data, polyOrder=None):
                            numCells[d]*(polyOrder+1)+1)
                for d in range(numDims)]
 
+    nodes = [-2.0/3, 0.0, 2.0/3]
     values = data.getValues()
-    intValues = np.zeros(np.int32(numCells*(polyOrder+1)))
+    intValues = np.zeros(np.int32(numCells*len(nodes)))
     intValues = intValues[..., np.newaxis]
 
-    # 1D hardcoded for the test purposes
-    intValues[0::int(polyOrder+1), 0] = expand_1d[int(polyOrder-1)](values, -2.0/3)
-    intValues[1::int(polyOrder+1), 0] = expand_1d[int(polyOrder-1)](values, 0)
-    intValues[2::int(polyOrder+1), 0] = expand_1d[int(polyOrder-1)](values, 2.0/3)
-
+    if numDims == 1:
+        for i, x in enumerate(nodes):
+            intValues[i::len(nodes), 0] = expand_1d[int(polyOrder-1)](values, x)
+        #end
+    elif numDims == 2:
+        for i, x in enumerate(nodes):
+            for j, y in enumerate(nodes):
+                intValues[i::len(nodes), j::len(nodes), 0] = expand_2d[int(polyOrder-1)](values, x, y)
+            #end
+        #end
+    #end
+        
     # Hardcoded stack 
     data.pushGrid(intGrid)
     data.pushValues(intValues)
