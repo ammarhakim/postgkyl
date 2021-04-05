508b5020a19d9abfd10fc3453eb59a1e116bd7b2
Petr Cagas <pcagas@vt.edu>
2021-04-05 18:16:21 -0400

    Adding an option to animate multiple tags in one figgure with -f0

--- postgkyl/commands/animate.py
+++ postgkyl/commands/animate.py
@@ -9,38 +9,35 @@ from postgkyl.data import Data
 import postgkyl.data.select as select
 from postgkyl.commands.util import vlog, pushChain
 
-def update(i, data, fig, kwargs):
-    # if kwargs['collected']:
-    #     grid, vals = select(ctx.obj['dataSets'][ctx.obj['sets'][0]],z0=i)
-    #     dat = Data()
-    #     dat.push(vals, grid=grid)
-    #     dat.frame = i
-    #     dat.time = grid[0][0]
-    # else:
-    #     dat = ctx.obj['dataSets'][ctx.obj['sets'][i]]
-    dat = data[i]
+def update(i, data, fig, offsets, kwargs):
     fig.clear()
-    #plt.clf()
-    kwargs['title'] = ''
     kwargs['figure'] = fig
-    if not kwargs['notitle']:
-        if dat.meta['frame'] is not None:
-            kwargs['title'] = kwargs['title'] + 'F: {:d} '.format(dat.meta['frame'])
+
+    for n in offsets:
+        dat = data[i+n]
+        kwargs['title'] = ''
+        if not kwargs['notitle']:
+            if dat.meta['frame'] is not None:
+                kwargs['title'] = kwargs['title'] + 'F: {:d} '.format(dat.meta['frame'])
+            #end
+            if dat.meta['time'] is not None:
+                kwargs['title'] = kwargs['title'] + 'T: {:.4e}'.format(dat.meta['time'])
+            #end
         #end
-        if dat.meta['time'] is not None:
-            kwargs['title'] = kwargs['title'] + 'T: {:.4e}'.format(dat.meta['time'])
+        if kwargs['arg'] is not None:
+            im = gplot(dat, kwargs['arg'], **kwargs)
+        else:
+            im = gplot(dat, **kwargs)
         #end
     #end
-    if kwargs['arg'] is not None:
-        return gplot(dat, kwargs['arg'], **kwargs)
-    else:
-        return gplot(dat, **kwargs)
-    #end
+    return(im)
 #end
 
 @click.command()
 @click.option('--use', '-u', default=None,
               help="Specify a tag to plot.")
+@click.option('--figure', '-f', default=None,
+              help="Specify figure (integer) to plot in.")
 @click.option('--squeeze', '-s', is_flag=True,
               help="Squeeze the components into one panel.")
 @click.option('--subplots', '-b', is_flag=True,
@@ -85,7 +82,7 @@ def update(i, data, fig, kwargs):
               help="Set maximal value of data for plots.")
 @click.option('--vmin', default=None, type=click.FLOAT,
               help="Set minimal value of data for plots.")
-@click.option('-f', '--float', is_flag=True,
+@click.option('--float', is_flag=True,
               help="Choose min/max levels based on current frame (i.e., each frame uses a different color range).")
 @click.option('--xlim', default=None, type=click.STRING,
               help="Set limits for the x-coordinate (lower,upper)")
@@ -155,6 +152,7 @@ def animate(ctx, **kwargs):
             if vmax < np.nanmax(val):
                 vmax = np.nanmax(val)
             #end
+        #end
         if kwargs['vmin'] is None:
             kwargs['vmin'] = vmin
             if kwargs['logz']:
@@ -169,12 +167,6 @@ def animate(ctx, **kwargs):
         #end
     #end
 
-    # if kwargs['collected']:
-    #     numSets = len(ctx.obj['dataSets'][ctx.obj['sets'][0]].getGrid()[0])
-    # else:
-    #     numSets = len(ctx.obj['sets'])
-    #numSets = ctx.obj['data'].getNumDatasets(kwargs['tag'])
-
     anims = []
     figs = []
     kwargs['legend'] = False
@@ -184,17 +176,41 @@ def animate(ctx, **kwargs):
         figsize = (int(kwargs['figsize'].split(',')[0]),
                    int(kwargs['figsize'].split(',')[1]))
     #end
+
+    offsets = [0]
+    tagIterator = list(data.tagIterator(kwargs['use']))
+    setFigure = False
+    minSize = np.NAN
+    if kwargs['figure']:
+        for tag in data.tagIterator(kwargs['use']):
+            numDatasets = int(data.getNumDatasets(tag=tag))
+            offsets.append(numDatasets)
+            minSize = int(np.nanmin((minSize, numDatasets)))
+        #end
+        offsets.pop()
+        tagIterator = list((kwargs['use'],))
+        kwargs['legend'] = True
+        setFigure = True
+        figNum = int(kwargs['figure'])
+    #end
     
-    for tag in data.tagIterator(kwargs['use']):
-        #numFiles = data.getNumDatasets(tag=tag, onlyActive=False)
+    for tag in tagIterator:
         dataList = list(data.iterator(tag=tag))
-        figs.append(plt.figure(figsize=figsize))
+        if setFigure:
+            figs.append(plt.figure(figNum, figsize=figsize))
+        else:
+            figs.append(plt.figure(figsize=figsize))
+        #end
         if not kwargs['saveframes']:
-            anims.append(FuncAnimation(figs[-1], update, len(dataList),
-                                       fargs=(dataList, figs[-1], kwargs),
+            anims.append(FuncAnimation(figs[-1], update, int(np.nanmin((minSize, len(dataList)))),
+                                       fargs=(dataList, figs[-1], offsets, kwargs),
                                        interval=kwargs['interval'], blit=False))
 
-            fName = 'anim_{:s}.mp4'.format(tag)
+            if tag is not None:
+                fName = 'anim_{:s}.mp4'.format(tag)
+            else:
+                fName = 'anim.mp4'
+            #end
             if kwargs['saveas']:
                 fName = str(kwargs['saveas'])
             #end
@@ -203,8 +219,8 @@ def animate(ctx, **kwargs):
                                fps=kwargs['fps'], dpi=kwargs['dpi'])
             #end
         else:
-            for i in range(len(dataList)):
-                update(i, dataList, figs[-1], kwargs)
+            for i in range(np.nanmin((minSize, len(dataList)))):
+                update(i, dataList, figs[-1], offsets, kwargs)
                 plt.savefig('{:s}_{:d}.png'.format(kwargs['saveframes'], i), dpi=kwargs['dpi'])
             #end
             kwargs['show'] = False #do not show in this case
