4a47eeae5ecc812bce24f0e071ea3d71cd935241
Petr Cagas <pcagas@vt.edu>
2021-04-19 13:20:50 -0400

    Adding option to print grid

--- postgkyl/commands/pr.py
+++ postgkyl/commands/pr.py
@@ -7,6 +7,8 @@ from postgkyl.commands.util import vlog, pushChain
 @click.command(help='Print the data')
 @click.option('--use', '-u',
               help='Specify a \'tag\' to apply to (default all tags).')
+@click.option('--grid', '-g', is_flag=True,
+              help='Print grid instead of values')
 @click.pass_context
 def pr(ctx, **kwargs):
     vlog(ctx, 'Starting pr')
@@ -14,7 +16,14 @@ def pr(ctx, **kwargs):
     data = ctx.obj['data']
     
     for dat in data.iterator(kwargs['use']):
-        click.echo(dat.getValues().squeeze())
+        if kwargs['grid']:
+            grid = dat.getGrid()
+            for g in grid:
+                click.echo(g)
+            #end
+        else:
+            click.echo(dat.getValues().squeeze())
+        #end
     #end
 
     vlog(ctx, 'Finishing pr')
