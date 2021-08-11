1b02fa6d77ebc4d7f302651e22fe2d44767f7129
Petr Cagas <pcagas@vt.edu>
2021-08-11 15:30:10 -0400

    Adding the build batch file

--- conda_push.sh
+++ conda_push.sh
@@ -9,17 +9,20 @@ conda config --set anaconda_upload yes
 conda build . --python=3.6
 conda build . --python=3.7
 conda build . --python=3.8
+#conda build . --python=3.9
 
 # Convert
 OS=$(uname)
 PKG36=$(conda build . --python=3.6 --output)
 PKG37=$(conda build . --python=3.7 --output)
 PKG38=$(conda build . --python=3.8 --output)
+#PKG39=$(conda build . --python=3.9 --output)
 if [ $OS="Linux" ]
 then 
     conda convert --platform osx-64 $PKG36
     conda convert --platform osx-64 $PKG37
     conda convert --platform osx-64 $PKG38
+#    conda convert --platform osx-64 $PKG39
     cd osx-64
     anaconda upload * --force
     cd ..
@@ -28,6 +31,7 @@ else
     conda convert --platform linux-64 $PKG36
     conda convert --platform linux-64 $PKG37
     conda convert --platform linux-64 $PKG38
+#    conda convert --platform linux-64 $PKG39
     cd linux-64
     anaconda upload * --force
     cd ..
