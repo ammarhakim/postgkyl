a9151cf37245cbc6ce47d686fd407bac65308c3f
Petr Cagas <p.cagas@gmail.com>
2024-07-30 13:37:49 +0200

    Adding first `plot` test

---
+++ tests/test_plot.py
@@ -0,0 +1,9 @@
+import os
+import matplotlib as mpl
+
+import postgkyl as pg
+
+def test_plot_pcolormesh():
+    data = pg.GData(f"{os.path.dirname(__file__)}/test_data/shock-f-ser-p1.gkyl")
+    img = pg.output.plot(data, )
+    assert isinstance(img, mpl.collections.QuadMesh)
