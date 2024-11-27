3038b38ab5f7b95c209de33ecd3ee401490a2c69
Petr Cagas <p.cagas@gmail.com>
2024-11-27 13:01:23 +0100

    Fixing an error in the adios tests. In addition, some of the test were rearanged to so they fail gracefully and don't cause all the other tests to fail as well.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -53,9 +53,10 @@ class TestCommands:
   def test_load(self):
     self.ctx.invoke(cmd.load)
     data = self.ctx.obj['data'].get_dataset(0)
-    np.testing.assert_array_equal(data.num_cells, (64, 32))
+    num_cells = data.num_cells
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    np.testing.assert_array_equal(num_cells, (64, 32))
 
 
   def test_ev_gkyl(self):
@@ -64,18 +65,18 @@ class TestCommands:
     self.ctx.invoke(cmd.ev, chain='f[0] f[0] +')
     data = self.ctx.obj['data'].get_dataset(0)
     values = data.get_values()
-    np.testing.assert_approx_equal(np.max(values), 3.352029)
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    np.testing.assert_approx_equal(np.max(values), 3.352029)
 
     # Check longer chain, substraction, and not using dataset id
     self.ctx.invoke(cmd.load)
     self.ctx.invoke(cmd.ev, chain='f f + f -')
     data = self.ctx.obj['data'].get_dataset(0)
     values = data.get_values()
-    np.testing.assert_approx_equal(np.max(values), 1.676014)
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    np.testing.assert_approx_equal(np.max(values), 1.676014)
 
     # Check tags
     self.ctx.invoke(cmd.load, tag='ts0')
@@ -83,9 +84,9 @@ class TestCommands:
     self.ctx.invoke(cmd.ev, chain='ts0 ts0 +')
     data = self.ctx.obj['data'].get_dataset(0, tag='ts0')
     values = data.get_values()
-    np.testing.assert_approx_equal(np.max(values), 3.3520293)
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    np.testing.assert_approx_equal(np.max(values), 3.3520293)
 
     # Check ev functionality on multiple dataset together
     self.ctx.invoke(cmd.load)
@@ -93,12 +94,12 @@ class TestCommands:
     self.ctx.invoke(cmd.ev, chain='f[:] 2 *')
     data0 = self.ctx.obj['data'].get_dataset(0)
     values0 = data0.get_values()
-    np.testing.assert_approx_equal(np.max(values0), 3.3520293)
     data1 = self.ctx.obj['data'].get_dataset(1)
-    values1 = data1.get_values()
-    np.testing.assert_approx_equal(np.max(values1), 3.3520293)
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    values1 = data1.get_values()
+    np.testing.assert_approx_equal(np.max(values0), 3.3520293)
+    np.testing.assert_approx_equal(np.max(values1), 3.3520293)
 
 
   @pytest.mark.skipif(adios_missing, reason="ADIOS2 is not installed")
@@ -106,42 +107,47 @@ class TestCommands:
     # Check metadata
     self.ctx.invoke(cmd.load)
     self.ctx.invoke(cmd.load)
-    self.ctx.invoke(cmd.ev, chain='f[1] f[1].charge *')
-    data = self.ctx.obj['data'].get_dataset(0)
+    self.ctx.invoke(cmd.load)
+    self.ctx.invoke(cmd.ev, chain='f[2] f[2].charge *')
+    data = self.ctx.obj['data'].get_dataset(2)
     values = data.get_values()
-    np.testing.assert_approx_equal(np.min(values), -1.676014)
-    # Check if metadata is properly passed through ev:
-    np.testing.assert_approx_equal(data.ctx["charge"], -1.0)
+    charge = data.ctx["charge"]
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    np.testing.assert_approx_equal(np.min(values), -1.676014)
+    # Check if metadata is properly passed through ev:
+    np.testing.assert_approx_equal(charge, -1.0)
 
 
   def test_interpolate(self):
     self.ctx.invoke(cmd.load)
     self.ctx.invoke(cmd.interpolate)
     data = self.ctx.obj['data'].get_dataset(0)
-    np.testing.assert_array_equal(data.num_cells, (192, 96))
+    num_cells = data.num_cells
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    np.testing.assert_array_equal(num_cells, (192, 96))
 
 
   def test_select(self):
     self.ctx.invoke(cmd.load)
     self.ctx.invoke(cmd.select, z0='0:10', z1='0.0', comp='0,3')
     data = self.ctx.obj['data'].get_dataset(0)
-    np.testing.assert_array_equal(data.values.shape, (10, 1, 2))
+    values_shape = data.values.shape
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    np.testing.assert_array_equal(values_shape, (10, 1, 2))
 
 
   def test_plot(self):
     self.ctx.invoke(cmd.load)
     self.ctx.invoke(cmd.plot, show=False)
     fig = plt.gcf()
-    assert fig.axes[0].get_ylabel() == "$z_1$"
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
+    label = fig.axes[0].get_ylabel()
     plt.close("all")
+    assert label == "$z_1$"
 
 
   @pytest.mark.skipif(ffmpeg_missing, reason="ffmpeg is not installed")
@@ -151,8 +157,9 @@ class TestCommands:
     fn = tmp_path / "test_anim.mp4"
     self.ctx.invoke(cmd.animate, show=False, saveas=fn)
     fig = plt.gcf()
-    assert fig.get_axes()[0].get_ylabel() == "$z_1$"
-    assert fn.exists()
+    label = fig.get_axes()[0].get_ylabel()
     self.ctx.obj['data'].clean()
     self.ctx.obj["in_data_strings_loaded"] = 0
     plt.close("all")
+    assert label == "$z_1$"
+    assert fn.exists()
