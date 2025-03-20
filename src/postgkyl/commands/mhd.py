64fef997157c5b1d636b26ffbfa200f31ab300e2
ammarhakim <a.hakim777@gmail.com>
2025-03-20 08:07:55 -0400

    The MHD command was not updates with refactored data reading and dataset management. Fixed now.

--- src/postgkyl/commands/mhd.py
+++ src/postgkyl/commands/mhd.py
@@ -32,47 +32,43 @@ def mhd(ctx, **kwargs):
   v = kwargs['variable_name']
   for dat in data.iterator(kwargs['use']):
     verb_print(ctx, 'mhd: Extracting {:s} from data set'.format(v))
-    overwrite = True
+    out = dat
     if kwargs['tag']:
       overwrite = False
       out = GData(tag=kwargs['tag'],
                   label=kwargs['label'],
                   comp_grid=ctx.obj['compgrid'],
                   ctx=dat.ctx)
-    #end
-    if v == "density":
-      grid, values = diag.get_density(dat, overwrite=overwrite)
-    elif v == "xvel":
-      grid, values = diag.get_vx(dat, overwrite=overwrite)
-    elif v == "yvel":
-      grid, values = diag.get_vy(dat, overwrite=overwrite)
-    elif v == "zvel":
-      grid, values = diag.get_vz(dat, overwrite=overwrite)
-    elif v == "vel":
-      grid, values = diag.get_vi(dat, overwrite=overwrite)
-    elif v == "Bx":
-      grid, values = diag.get_mhd_Bx(dat, overwrite=overwrite)
-    elif v == "By":
-      grid, values = diag.get_mhd_By(dat, overwrite=overwrite)
-    elif v == "Bz":
-      grid, values = diag.get_mhd_Bz(dat, overwrite=overwrite)
-    elif v == "Bi":
-      grid, values = diag.get_mhd_Bi(dat, overwrite=overwrite)
-    elif v == "magpressure":
-      grid, values = diag.get_mhd_mag_p(dat, gasGamma=kwargs['gas_gamma'], mu0=kwargs['mu0'], overwrite=overwrite)
-    elif v == "pressure":
-      grid, values = diag.get_mhd_p(dat, gasGamma=kwargs['gas_gamma'], mu0=kwargs['mu0'], overwrite=overwrite)
-    elif v == "temp":
-      grid, values = diag.get_mhd_temp(dat, gasGamma=kwargs['gas_gamma'], mu0=kwargs['mu0'], overwrite=overwrite)
-    elif v == "sound":
-      grid, values = diag.get_mhd_sound(dat, gasGamma=kwargs['gas_gamma'], mu0=kwargs['mu0'], overwrite=overwrite)
-    elif v == "mach":
-      grid, values = diag.get_mhd_mach(dat, gasGamma=kwargs['gas_gamma'], mu0=kwargs['mu0'], overwrite=overwrite)
-    #end
-    if kwargs['tag']:
-      out.push(grid, values)
       data.add(out)
     #end
-  #end
+    if v == "density":
+      diag.get_density(dat, out_mom=out)
+    elif v == "xvel":
+      diag.get_vx(dat, out_mom=out)
+    elif v == "yvel":
+      diag.get_vy(dat, out_mom=out)
+    elif v == "zvel":
+      diag.get_vz(dat, out_mom=out)
+    elif v == "vel":
+      diag.get_vi(dat, out_mom=out)
+    elif v == "Bx":
+      diag.get_mhd_Bx(dat, out_mom=out)
+    elif v == "By":
+      diag.get_mhd_By(dat, out_mom=out)
+    elif v == "Bz":
+      diag.get_mhd_Bz(dat, out_mom=out)
+    elif v == "Bi":
+      diag.get_mhd_Bi(dat, out_mom=out)
+    elif v == "magpressure":
+      diag.get_mhd_mag_p(dat, mu_0=kwargs['mu0'], out_mom=out)
+    elif v == "pressure":
+      diag.get_mhd_p(dat, gas_gamma=kwargs['gas_gamma'], mu_0=kwargs['mu0'], out_mom=out)
+    elif v == "temp":
+      diag.get_mhd_temp(dat, gas_gamma=kwargs['gas_gamma'], mu_0=kwargs['mu0'], out_mom=out)
+    elif v == "sound":
+      diag.get_mhd_sound(dat, gas_gamma=kwargs['gas_gamma'], mu_0=kwargs['mu0'], out_mom=out)
+    elif v == "mach":
+      diag.get_mhd_mach(dat, gas_gamma=kwargs['gas_gamma'], mu_0=kwargs['mu0'], out_mom=out)
+    #end
   verb_print(ctx, 'Finishing mhd')
 #end
